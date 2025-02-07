76e89102a03089afd72d9ef87ce87748fb5b0823
“MiriamLearn <miri67216@gmail.com>
2025-02-07 02:22:21 +0200

    change 5

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -32,15 +32,15 @@ builder.Services.AddCors(options =>
 });
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API v1");
         options.RoutePrefix = string.Empty; // אם אתה רוצה ש-Swagger יופיע בדף הראשי
     });
-}
+//}
 
 app.UseCors("AllowAll");
 
